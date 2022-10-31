f2fb224b4cfb1966cbd17e00899905289415e4c1
TomGieben <t.gieben@outlook.com>
2022-10-31 13:32:36 +0100

    database name changed

--- configs/database.php
+++ configs/database.php
@@ -3,7 +3,7 @@ return [
     'DB_CONNECTION' => 'mysql',
     'DB_HOST' => '127.0.0.1',
     'DB_PORT' => '3306',
-    'DB_DATABASE' => 'cubedupe',
+    'DB_DATABASE' => 'database',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
 ];
